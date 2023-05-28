39cb7ee5d12018325b315c0130b50017a496878f
Matthews R. Sampaio <85257122+matthewsrsampaio@users.noreply.github.com>
2023-05-28 13:44:07 -0300

    fix and a few add try catch

--- src/main/java/bean/ComidaBean.java
+++ src/main/java/bean/ComidaBean.java
@@ -17,8 +17,12 @@ public class ComidaBean {
 	private Comida messageComida;
 		
 	public String salvar() {
-		ComidaDao.salvar(comida);
-		return null;
+		try {
+			ComidaDao.salvar(comida);
+			return null;
+		}catch(Exception e) {
+			return "Bucho em ComidaBean.salvar() -> " + e;
+		}
 	}
 	
 	public List<Comida> buscarTodos() {
@@ -27,27 +31,39 @@ public class ComidaBean {
 	}
 	
 	public String deletar() {
-		ComidaDao.deletar(comida);
-		listaComida = ComidaDao.buscarTodos();
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, " =>", "Deletado com sucesso!"));
-		return null;
+		try {
+			ComidaDao.deletar(comida);
+			listaComida = ComidaDao.buscarTodos();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, " =>", "Deletado com sucesso!"));
+			return null;
+		}catch(Exception e) {
+			return "Bucho em ComidaBean.deletar() -> " + e;
+		}
 	}
 	
 	public void infoItem(Comida comida) {
-		String texto = ComidaDao.buscarPorId(comida.getId()).toString();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, " Info =>", texto));
+		try {
+			String texto = ComidaDao.buscarPorId(comida.getId()).toString();
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, " Info =>", texto));
+		}catch(Exception e) {
+			System.out.println("Bucho em ComidaBean.infoItem()" + e);
+		}
 	}
 	
 	public void info() {
-		listaComida = ComidaDao.buscarTodos();
-		Integer maiorId = Integer.MIN_VALUE;
-		for(Comida objComida :  listaComida) {
-			if(objComida.getId() > maiorId) {
-				maiorId = objComida.getId();
+		try {
+			listaComida = ComidaDao.buscarTodos();
+			Integer maiorId = Integer.MIN_VALUE;
+			for(Comida objComida :  listaComida) {
+				if(objComida.getId() > maiorId) {
+					maiorId = objComida.getId();
+				}
 			}
+			String texto = ComidaDao.buscarPorId(maiorId).toString();
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Informação sobre o Objeto com maior ID => ", texto));
+		}catch(Exception e) {
+			System.out.println("Bucho em ComidaBean.info() " + e);
 		}
-		String texto = ComidaDao.buscarPorId(maiorId).toString();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Informação sobre o Objeto com maior ID => ", texto));
     }
 	
 	//Getter and Setters
